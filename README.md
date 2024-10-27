b5e80efee44796d819f50814b5c47fa3a6a75b6a
Glitch752 <xxGlitch752xx@gmail.com>
2024-10-26 23:21:21 -0500

    Start implementing rules

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Slack Channel Chameleon
-"Chameleon" comes from the idea that the bot always adapts based on the messages sent to the channel. It's not a great name, but I couldn't think of anything better.  
-  
+I'm not sure what I'm doing with this project yet, but here's the concept:
+
 This is a Slack bot that hosts a game around restricting what messages you can send in a particular channel.  
-The game rules are still a work in progress; the project is mostly so I can learn how to write a Slack bot.  
+The game rules are still a work in progress; I'm mostly making it so I can learn how bolt works. I want to find an interesting twist to create an incentive to send messages, but this is mostly a proof of concept for now.  
+
+"Chameleon" comes from the idea that the bot always adapts based on the messages sent to the channel. It's not a very good name, but I couldn't think of anything better.  
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { App } from '@slack/bolt';
+import { App, SayFn } from '@slack/bolt';
+import topThousandWords from "./topThousandEnglishWords.json";
 
 const app = new App({
   signingSecret: process.env.SLACK_SIGNING_SECRET,
@@ -6,9 +7,67 @@ const app = new App({
   clientId: process.env.SLACK_CLIENT_ID,
 });
 
-app.command('/chameleon-leaderboard', async ({ command, ack, say }) => {
+type Rule = {
+  id: string,
+  name: string,
+  description: string,
+  check: (message: string) => boolean | Promise<boolean>,
+  conflictsWith?: string[]
+};
+
+const rules: Rule[] = [{
+  id: "all-lowercase",
+  name: "Lowercase messages",
+  description: "all messages must be in lowercase. stay calm.",
+  check: (message: string) => message === message.toLowerCase(),
+  conflictsWith: ["all-uppercase"],
+}, {
+  id: "all-uppercase",
+  name: "Uppercase messages",
+  description: "ALL MESSAGES MUST BE IN UPPERCASE. SHOUTING IS ENCOURAGED!",
+  check: (message: string) => message === message.toUpperCase(),
+  conflictsWith: ["all-lowercase"],
+}, {
+  id: "only-emojis",
+  name: "Only emojis",
+  description: ":rocket: :100: :fire:",
+  check: (message: string) => message.split(" ").every(word => word.startsWith(":") && word.endsWith(":")), // TODO: This isn't a proper check
+}, {
+  id: "unique-messages",
+  name: "Unique messages",
+  description: "Be original! Messages must have never been sent in this Slack workspace before.",
+  check: async (message: string) => {
+    const result = await app.client.search.messages({
+      query: message
+    });
+    return (result.messages?.total ?? 0) === 0;
+  }
+}, {
+  id: "no-common-words",
+  name: "No common words",
+  description: "Cultivate novelty! Messages exclude English's 1000 commonest vocables.",
+  check: (message: string) => {
+    const words = message.split(/[\s\-.,!?]+/);
+    return words.every(word => !topThousandWords.includes(word.toLowerCase()));
+  }
+}];
+
+const activeRules = new Set<string>();
+// TEMPORARY: we hard-code the rule to be active
+activeRules.add("unique-messages");
+
+function getRulesMessage(): string {
+  let message = `${activeRules.size}/${rules.length} rules are currently active:\n`;
+  rules.forEach(rule => {
+    const emoji = activeRules.has(rule.id) ? ":tw_white_check_mark:" : ":tw_x:";
+    message += `${emoji} ${rule.name}: ${rule.description}\n`;
+  });
+  return message;
+}
+
+app.command('/chameleon-rules', async ({ command, ack, say }) => {
   await ack();
-  await say(`Hello, <@${command.user_id}>!`);
+  await say(getRulesMessage());
 });
 
 (async () => {
